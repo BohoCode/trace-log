4feb653f086a188c2b9c0186ce1807f06b9ab07f
jamiebowen <jamie.bowen@forgerock.com>
2016-10-13 00:35:52 +0100

    Inital functionality and tests

---
+++ test/trace-log.spec.js
@@ -0,0 +1,98 @@
+/*
+ *
+ * Copyright (c) 2016 Jamie Bowen
+ *
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+ * the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ * OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+
+var expect = require('chai').expect;
+var Logger = require("../trace-log");
+var sinon  = require("sinon");
+
+
+const LIB_NAME = "TestLibrary";
+const TEST_LOG_STRING = "Test log string";
+const TEST_LOG_NAME = "TestModule";
+const SUB_MODULE_NAME = "SubModule";
+
+/*jshint esversion: 6 */
+describe('setGlobalDefaults()', function () {
+
+    beforeEach(function() {
+        sinon.stub(console, "log").returns(void 0);
+        sinon.stub(console, "error").returns(void 0);
+    });
+
+    afterEach(function() {
+        console.log.restore();
+        console.error.restore();
+    });
+
+    it("info log messages should be written when global default log level is set to INFO", function(){
+        Logger.setGlobalDefaults(LIB_NAME, Logger.LEVEL.INFO);
+        var logger = new Logger(TEST_LOG_NAME);
+        logger.info(TEST_LOG_STRING);
+        expect(console.log.called).to.be.true;
+        expect(console.log.getCall(0).args[0]).to.contain(TEST_LOG_STRING);
+        expect(console.log.getCall(0).args[0]).to.contain(LIB_NAME);
+        expect(console.log.getCall(0).args[0]).to.contain(TEST_LOG_NAME);
+        expect(console.log.getCall(0).args[0]).to.contain("[INFO]")
+    });
+
+    it("error log messages should be written when global default log level is set to INFO", function(){
+        Logger.setGlobalDefaults(LIB_NAME, Logger.LEVEL.INFO);
+        var logger = new Logger(TEST_LOG_NAME);
+        logger.error(TEST_LOG_STRING);
+        expect(console.error.called).to.be.true;
+        expect(console.error.getCall(0).args[0]).to.contain(TEST_LOG_STRING);
+        expect(console.error.getCall(0).args[0]).to.contain(LIB_NAME);
+        expect(console.error.getCall(0).args[0]).to.contain(TEST_LOG_NAME);
+        expect(console.error.getCall(0).args[0]).to.contain("[ERROR]")
+    });
+
+    it("debug log messages should NOT be written when global default log level is set to INFO", function(){
+        Logger.setGlobalDefaults(LIB_NAME, Logger.LEVEL.INFO);
+        var logger = new Logger(TEST_LOG_NAME);
+        logger.debug(TEST_LOG_STRING);
+        expect(console.error.called).to.be.false;
+        expect(console.log.called).to.be.false;
+    });
+
+    it("debug log messages should be written when global default log level is set to INFO but local logger is TRACE", function(){
+        Logger.setGlobalDefaults(LIB_NAME, Logger.LEVEL.INFO);
+        var logger = new Logger(TEST_LOG_NAME, Logger.LEVEL.TRACE);
+        logger.debug(TEST_LOG_STRING);
+        expect(console.log.called).to.be.true;
+        expect(console.log.getCall(0).args[0]).to.contain(TEST_LOG_STRING);
+        expect(console.log.getCall(0).args[0]).to.contain(LIB_NAME);
+        expect(console.log.getCall(0).args[0]).to.contain(TEST_LOG_NAME);
+        expect(console.log.getCall(0).args[0]).to.contain("[DEBUG]")
+    });
+
+    it("includes the subModuleName when obtaining a sub logger from a logger.", function(){
+        Logger.setGlobalDefaults(LIB_NAME, Logger.LEVEL.INFO);
+        var logger = new Logger(TEST_LOG_NAME, Logger.LEVEL.TRACE);
+        var subModuleLogger = logger.getSubModuleLogger(SUB_MODULE_NAME);
+        subModuleLogger.debug(TEST_LOG_STRING);
+        expect(console.log.called).to.be.true;
+        expect(console.log.getCall(0).args[0]).to.contain(TEST_LOG_STRING);
+        expect(console.log.getCall(0).args[0]).to.contain(LIB_NAME);
+        expect(console.log.getCall(0).args[0]).to.contain(TEST_LOG_NAME);
+        expect(console.log.getCall(0).args[0]).to.contain(SUB_MODULE_NAME);
+        expect(console.log.getCall(0).args[0]).to.contain("[DEBUG]")
+    });
+
+});
---
+++ trace-log.js
@@ -0,0 +1,109 @@
+/*
+ *
+ * Copyright (c) 2016 Jamie Bowen
+ *
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+ * the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ * OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/*jshint esversion: 6 */
+var vsprintf = require("sprintf-js").vsprintf;
+
+class Logger {
+
+    static setGlobalDefaults(libraryName, logLevel) {
+        Logger.defaultLogLevel = logLevel;
+        Logger.libraryName = libraryName;
+        Logger.debug = require('debug')('libraryName');
+    };
+
+    constructor(moduleName, localLogLevel){
+        this.moduleName = moduleName;
+        if(typeof localLogLevel !== 'undefined' && localLogLevel >= Logger.LEVEL.FATAL && localLogLevel <= Logger.LEVEL.TRACE) {
+            this.localLogLevel = localLogLevel;
+        }
+    }
+
+    getSubModuleLogger(subModuleName, localLogLevel){
+        var catSubModuleName = this.moduleName + ">" + subModuleName;
+        var subModuleLogLevel = this.localLogLevel;
+        if(typeof localLogLevel !== 'undefined'){
+            subModuleLogLevel = localLogLevel;
+        }
+        return new Logger(catSubModuleName, subModuleLogLevel);
+    }
+
+    trace(template, args){
+        this.makeLogEntry(Logger.LEVEL.TRACE, template, args);
+    }
+
+    debug(template, args){
+        this.makeLogEntry(Logger.LEVEL.DEBUG, template, args);
+    }
+
+    info(template, args){
+        this.makeLogEntry(Logger.LEVEL.INFO, template, args);
+    }
+
+    warn(template, args){
+        this.makeLogEntry(Logger.LEVEL.WARN, template, args);
+    }
+
+    error(template, args){
+        this.makeLogEntry(Logger.LEVEL.ERROR, template, args);
+    }
+
+    fatal(template, args){
+        this.makeLogEntry(LEVEL.FATAL, template, args);
+    }
+
+    makeLogEntry(logLevel, template, args){
+        var validLogLevel = Logger.defaultLogLevel;
+        if(typeof this.localLogLevel !== 'undefined'){
+            validLogLevel = this.localLogLevel;
+        }
+        if(logLevel <= validLogLevel){
+
+            var providedLogMessage = vsprintf(template, args);
+            var strLevel = "[" + this.getStringLogLevel(logLevel) + "] ";
+            if(logLevel <= Logger.LEVEL.ERROR){
+                console.error(strLevel + Logger.libraryName + " " + this.moduleName + ": " + providedLogMessage);
+            }else{
+                console.log(strLevel + Logger.libraryName + " " + this.moduleName + ": " + providedLogMessage);
+            }
+        }
+    }
+
+    getStringLogLevel(dbgValue){
+        for (var name in Logger.LEVEL){
+            if(Logger.LEVEL[name] === dbgValue){
+                return name;
+            }
+        }
+    }
+}
+
+Logger.LEVEL = {
+    FATAL: 0,
+    ERROR: 1,
+    WARN: 2,
+    INFO: 3,
+    DEBUG: 4,
+    TRACE: 5
+};
+
+Logger.defaultLogLevel = Logger.LEVEL.DEBUG;
+Logger.libraryName = "call Logger.globalValues() to set library name and global log level";
+
+module.exports = Logger;
